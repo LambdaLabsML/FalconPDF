59d2e669a6e6e4785d1cf846b4017a6859b90b6f
Xi Tian <tian.xi@outlook.com>
2023-07-21 17:37:11 +0100

    Change ChromaDB version, otherwise it needs higher sqlite version

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import os
-os.system("sudo apt update")
-os.system("sudo apt install --only-upgrade sqlite3")  # for Chroma DB
+import os# for Chroma DB
 os.system(f"git lfs install")
 os.system(f"pip install torch torchvision torchaudio torchtext torchdata --extra-index-url https://download.pytorch.org/whl/cu118 -U")
-os.system(f"pip install bitsandbytes sentencepiece fsspec gradio langchain einops scipy unstructured xformers sentence-transformers chromadb -U")
+os.system(f"pip install bitsandbytes sentencepiece fsspec gradio langchain einops scipy unstructured xformers sentence-transformers chromadb==0.3.29")
 os.system(f"pip install git+https://github.com/huggingface/transformers.git -U")
 os.system(f"pip install git+https://github.com/huggingface/accelerate.git -U")
 os.system(f"pip install pydantic==1.10.11")
