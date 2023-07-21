17e17ba9bfb18035f1f4a8d107275085685b0256
Xi Tian <tian.xi@outlook.com>
2023-07-21 15:49:51 +0100

    update sqlite3 for Chroma DB

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+os.system("sudo apt update")
+os.system("sudo apt install --only-upgrade sqlite3")  # for Chroma DB
 os.system(f"git lfs install")
 os.system(f"pip install torch torchvision torchaudio torchtext torchdata --extra-index-url https://download.pytorch.org/whl/cu118 -U")
 os.system(f"pip install bitsandbytes sentencepiece fsspec gradio langchain einops scipy unstructured xformers sentence-transformers chromadb -U")
